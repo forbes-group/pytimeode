85dedcb1268d711c22f46b9760bfa4d6858846c8
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2015-08-11 09:21:24 -0700

    VER: Started work on version 0.7
    --HG--
    branch : 0.7

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class test(original_test):
                     raise
 
 setup(name=NAME,
-      version='0.6',
+      version='0.7dev',
       packages=find_packages(exclude=['tests']),
       cmdclass=dict(test=test),
 
